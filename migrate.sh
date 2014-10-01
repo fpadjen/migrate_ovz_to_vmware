98dd1e2ca00d144fe7d2e57be679a39294c07ef2
Frederik Padjen <frederik.padjen@elitepartner.de>
2014-10-01 17:33:35 +0200

    syntax fix

--- migrate.sh
+++ migrate.sh
@@ -50,7 +50,7 @@ LOOPDEV=`mount | grep $WORKINGDIR/$ID | awk '{print $3}'`
 rsync --numeric-ids -avP /var/lib/vz/private/$ID/* $WORKINGDIR/$ID/
 sed -i -e s/127.0.0.1/127.1.1.1/g $WORKINGDIR/$ID/etc/network/interfaces
 sed -i -e s/venet0:0/eth0/g $WORKINGDIR/$ID/etc/network/interfaces
-echo `blkid $WORKINGDIR/$ID.img | awk '{print $2}'` / ext4 errors=remount-ro 0 1 >> $WORKINGDIR/$ID/etc/fstab`
+echo `blkid $WORKINGDIR/$ID.img | awk '{print $2}'` / ext4 errors=remount-ro 0 1 >> $WORKINGDIR/$ID/etc/fstab
 
 echo "Installing grub and kernel"
 for i in dev sys proc ; do mount --bind /$i $WORKINGDIR/$ID/$i
